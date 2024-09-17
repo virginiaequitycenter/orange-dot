b1eb3e0ce735391eb1017ef572a24616f5409e5e
eam5 <eam5hc@virginia.edu>
2024-09-17 11:47:52 -0400

    update sss

--- OrangeDot6_files/scripts/self_sufficiency_standard.R
+++ OrangeDot6_files/scripts/self_sufficiency_standard.R
@@ -15,16 +15,53 @@ setwd(here("OrangeDot6_files"))
 county_codes <- c("003", "540", "065", "079", "109", "125", "029")
 localities <- c("Albemarle County", "Buckingham County", "Charlottesville city", "Fluvanna County", "Greene County", "Louisa County", "Nelson County")
 
-
 # Read data
-self_suff_sheet <- read_excel("data/tempdata/VA2021_all_families.xlsx", sheet = "By Family") %>% 
+self_suff_byfamily <- read_excel("data/tempdata/VA2021_all_families.xlsx", sheet = "By Family") %>% 
   clean_names()
 
-self_suff_county <- self_suff_sheet %>% 
+# Family Composition: Single Parent + 1 preschooler + 1 school age
+self_suff_a1p1s1 <- self_suff_byfamily %>% 
   filter(county %in% localities) %>% 
-  filter(family_type == "a1i0p1s1t0")
+  filter(family_type == "a1i0p1s1t0") %>% 
+  rename_with(~paste0(. , "_a1p1s1"), 11:24)
 
-mean_ssw <- mean(self_suff_county$annual_self_sufficiency_wage)
+# SSS Mean across family compositions (Adults: 1|2, Total Children (all age): 1|2)
+self_suff_mean <- self_suff_byfamily %>% 
+  filter(county %in% localities) %>% 
+  mutate_at(c("adult_s", "infant_s","preshooler_s", "schoolager_s", "teenager_s"), as.numeric) %>% 
+  mutate(total_child = rowSums(across(infant_s:teenager_s), na.rm =TRUE)) %>% 
+  filter(adult_s <= 2 & total_child <= 2) %>%
+  group_by(county, state, year) %>% 
+  summarise(housing_costs = mean(housing_costs),
+            child_care_costs = mean(child_care_costs),
+            food_costs = mean(food_costs),
+            transportation_costs = mean(transportation_costs),
+            health_care_costs = mean(health_care_costs),
+            miscellaneous_costs = mean(miscellaneous_costs),
+            taxes = mean(taxes),
+            earned_income_tax_credit = mean(earned_income_tax_credit),
+            child_tax_credit = mean(child_tax_credit),
+            hourly_self_sufficiency_wage = mean(hourly_self_sufficiency_wage),
+            monthly_self_sufficiency_wage = mean(monthly_self_sufficiency_wage),
+            annual_self_sufficiency_wage = mean(annual_self_sufficiency_wage),
+            emergency_savings = mean(emergency_savings)) %>% 
+  rename_with(~paste0(. , "_mean"), 4:16)
+  
+
+
+mean_ssw <- mean(self_suff_mean$annual_self_sufficiency_wage_mean)
+
+# Join tables
+self_suff_county <- self_suff_a1p1s1 %>% 
+  left_join(self_suff_mean, by = join_by(state, year, county)) %>% 
+  select(state, year, county, housing_costs_a1p1s1, housing_costs_mean, child_care_costs_a1p1s1, child_care_costs_mean,
+         food_costs_a1p1s1, food_costs_mean, transportation_costs_a1p1s1, transportation_costs_mean,
+         health_care_costs_a1p1s1, health_care_costs_mean, miscellaneous_costs_a1p1s1, miscellaneous_costs_mean,
+         taxes_a1p1s1, taxes_mean, hourly_self_sufficiency_wage_a1p1s1, hourly_self_sufficiency_wage_mean,
+         monthly_self_sufficiency_wage_a1p1s1, monthly_self_sufficiency_wage_mean, 
+         annual_self_sufficiency_wage_a1p1s1, annual_self_sufficiency_wage_mean)
+
+
 
 # Write CSV
 write_csv(self_suff_county, "data/self_suff_county_2021.csv")
